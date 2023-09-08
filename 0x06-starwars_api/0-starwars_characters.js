f3259f52d1ca97adad1ac49ededcb066a6802f18
kelechi <kelechichukwuma930@gmail.com>
2023-09-08 14:03:19 +0100

    refactored code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,26 @@
 #!/usr/bin/node
-
+const argv = process.argv;
 const request = require('request');
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+function getPromise (url) {
+  const promise = new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (err) reject(err);
+      else resolve(JSON.parse(body).name);
+    });
+  });
+  return promise;
+}
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
+request(
+  `https://swapi-api.hbtn.io/api/films/${argv[2]}`,
+  async function (err, res, body) {
+    if (err) throw err;
+    const data = JSON.parse(body).characters;
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
-
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
+    for (const i of data) {
+      const result = await getPromise(i);
+      console.log(result);
     }
   }
-};
-
-getCharNames();
+);
